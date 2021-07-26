7d69a5df0170c5edaf654d913fa615d98c31b47e
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-26 12:36:16 +0200

    Initial Commit

---
+++ firstSwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstSwiftUIProject
+//
+//  Created by Macbook on 26/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstSwiftUIProject/firstSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIProjectApp.swift
+//  firstSwiftUIProject
+//
+//  Created by Macbook on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
